90a1e5e45d8f698b32e22a8fdf74255cab8df33e
jacobreesgit <jacobrees@me.com>
2025-05-15 10:14:03 +0100

    a

---
+++ MusicMemoryWidgets/NowPlayingAttributes.swift
@@ -0,0 +1,32 @@
+//
+//  NowPlayingAttributes.swift
+//  Music Memory
+//
+//  Created by Jacob Rees on 15/05/2025.
+//
+
+
+import ActivityKit
+import SwiftUI
+
+public struct NowPlayingAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        public var title: String
+        public var artist: String
+        public var playCount: Int
+        public var playbackProgress: Double
+        public var isPlaying: Bool
+        public var artworkData: Data?
+        
+        public init(title: String, artist: String, playCount: Int, playbackProgress: Double, isPlaying: Bool, artworkData: Data? = nil) {
+            self.title = title
+            self.artist = artist
+            self.playCount = playCount
+            self.playbackProgress = playbackProgress
+            self.isPlaying = isPlaying
+            self.artworkData = artworkData
+        }
+    }
+    
+    public init() { }
+}
